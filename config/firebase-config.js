2655f22b183022560f4e63916da766b8ccaf7dca
Billy <xzsa654@gmail.com>
2025-01-12 20:38:33 +0800

    firebase

---
+++ config/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD1Fcz0_porOUzC5SQiV0et_g7oXLI8PI4',
+  authDomain: 'eventurearts-4da06.firebaseapp.com',
+  projectId: 'eventurearts-4da06',
+  storageBucket: 'eventurearts-4da06.firebasestorage.app',
+  messagingSenderId: '741345772798',
+  appId: '1:741345772798:web:14ae875e868ffee483d1af',
+  measurementId: 'G-YLYK4MVY86',
+}
+
+export default firebaseConfig
