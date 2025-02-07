8f23595cb75a03e45bc3ed60068bdc944b4db146
=xzsa654 <=xzsa654@gmail.com>
2025-02-07 18:27:36 +0800

    添加預設樣式，改滾動條

--- app/page.js
+++ app/page.js
@@ -38,10 +38,11 @@ export default function Home() {
       <hr />
       <h3>secondary</h3>
       <Button color="secondary">Default</Button>
-
-      {colorShow('secondary')}
-      <hr />
       <h3>yellow</h3>
+      <Button className="bg-green">Default</Button>
+      {colorShow('green', false)}
+      <hr />
+      <h3>green</h3>
       <Button className="bg-yellow">Default</Button>
 
       {colorShow('yellow', false)}
@@ -63,16 +64,46 @@ export default function Home() {
       <h1 className="text-6xl mb-2 font-logo">EVENTUREARTS(font-logo)</h1>
       <h1 className="text-6xl mb-2 font-kanit">EVENTUREARTS(font-kanit)</h1>
       <h1 className="text-6xl mb-2 font-unna">EVENTUREARTS(font-unna)</h1>
-      {/* <Button className="bg-yellow-50">Default</Button>
-      <Button className="bg-yellow-100">Default</Button>
-      <Button className="bg-yellow-200">Default</Button>
-      <Button className="bg-yellow-300">Default</Button>
-      <Button className="bg-yellow-400">Default</Button>
-      <Button className="bg-yellow-500">Default</Button>
-      <Button className="bg-yellow-600">Default</Button>
-      <Button className="bg-yellow-700">Default</Button>
-      <Button className="bg-yellow-800">Default</Button>
-      <Button className="bg-yellow-900">Default</Button> */}
+      <p>----------------------------------------------</p>
+      <h1 className="text-6xl mb-2 font-unna tracking-wide">
+        EVENTUREARTS(font-unna)
+      </h1>
+      <h1 className="text-6xl mb-2 font-unna tracking-[.2em]">
+        EVENTUREARTS(font-unna)
+      </h1>
+      <h1 className="mb-2 text-6xl font-cn tracking-[.2em]">
+        字型測試(font-cn)
+      </h1>
+      <h1 className="mb-2 text-6xl font-cn">字型測試(font-cn)</h1>
+      <h1 className="mb-2 text-6xl font-cn ">字型測試(font-cn)</h1>
+      <h1 className="text-12">12313213</h1>
+      <h1 className="text-16">12313213</h1>
+      <h1>123</h1>
+      <p className="text-3xl">測試P</p>
+      <p className="">測試P</p>
+      <div>54456</div>
+      <div
+        className=" h-80 border border-5 overflow-scroll
+"
+      >
+        <h1>
+          Loremredgrr ipsum dolor, sit amet consectetur adipisicing elit.
+          Corporis ipsum esse nam, impedit vitae eius ipsam itaque incidunt sit
+          minima aliquid dolores nisi quasi reiciendis, pariatur in. Placeat,
+          doloremque.
+        </h1>
+        Perferendis.
+      </div>
+      <Button className="bg-secondary-50">Default</Button>
+      <Button className="bg-secondary-100">Default</Button>
+      <Button className="bg-secondary-200">Default</Button>
+      <Button className="bg-secondary-300">Default</Button>
+      <Button className="bg-secondary-400">Default</Button>
+      <Button className="bg-secondary-500">Default</Button>
+      <Button className="bg-secondary-600">Default</Button>
+      <Button className="bg-secondary-700">Default</Button>
+      <Button className="bg-secondary-800">Default</Button>
+      <Button className="bg-secondary-900">Default</Button>
     </>
   )
 }
