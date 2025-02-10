bb25bd99f1c07aa880f4e475f24ac33c9b181f75
blair <carryhyo@gmail.com>
2025-02-10 15:18:41 +0800

    merge

---
+++ app/course/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+export default function CoursePage(props) {
+  return (
+    <>
+      <div>Course Pagefff</div>
+      
+    </>
+  )
+}
