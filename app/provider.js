94d4844a3ed935470ae6eb1689093819cd604e24
lichia <lily850413@gmail.com>
2025-02-27 14:50:50 +0800

    1

--- app/provider.js
+++ app/provider.js
@@ -3,14 +3,19 @@
 import { AuthContextProvider } from '@/contexts/auth-context'
 import { HeroUIProvider, ToastProvider } from '@heroui/react'
 import { ModalProvider } from '@/contexts/modal-context'
-
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 export default function Provider({ children }) {
   return (
     <HeroUIProvider>
-      <AuthContextProvider>
+      <GoogleReCaptchaProvider
+        reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+        language="zh-TW"
+      >
         <ToastProvider placement="top-right" />
-        <ModalProvider>{children}</ModalProvider>
-      </AuthContextProvider>
+        <AuthContextProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </AuthContextProvider>
+      </GoogleReCaptchaProvider>
     </HeroUIProvider>
   )
 }
