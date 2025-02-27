9155685c3170cd8f4a40d646e0c40e8d3956142d
lichia <lily850413@gmail.com>
2025-02-27 14:00:14 +0800

    改好provider

--- app/provider.js
+++ app/provider.js
@@ -3,7 +3,7 @@
 import { AuthContextProvider } from '@/contexts/auth-context'
 import { HeroUIProvider, ToastProvider } from '@heroui/react'
 import { ModalProvider } from '@/contexts/modal-context'
-import { OrderContextProvider } from '@/contexts/order-context'
+
 export default function Provider({ children }) {
   return (
     <HeroUIProvider>
