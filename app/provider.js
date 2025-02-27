305a52f14d0f190e81b0fd4edc7c4fdf5c0a8902
lichia <lily850413@gmail.com>
2025-02-27 10:48:23 +0800

    刪除未使用的 "useorder" hook

--- app/provider.js
+++ app/provider.js
@@ -8,10 +8,8 @@ export default function Provider({ children }) {
   return (
     <HeroUIProvider>
       <AuthContextProvider>
-      <ToastProvider placement="top-right" />
-      <OrderContextProvider>
+        <ToastProvider placement="top-right" />
         <ModalProvider>{children}</ModalProvider>
-        </OrderContextProvider>
       </AuthContextProvider>
     </HeroUIProvider>
   )
