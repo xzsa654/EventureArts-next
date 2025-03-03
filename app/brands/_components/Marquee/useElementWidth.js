d1405a1d8ff2054218129951e74e803a882ee0c9
lichia <lily850413@gmail.com>
2025-03-04 02:51:04 +0800

    商家資訊頁+跑馬燈做好

---
+++ app/brands/_components/Marquee/useElementWidth.js
@@ -0,0 +1,18 @@
+import { useState, useLayoutEffect } from "react";
+
+export function useElementWidth(ref) {
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    function updateWidth() {
+      if (ref.current) {
+        setWidth(ref.current.offsetWidth);
+      }
+    }
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, [ref]);
+
+  return width;
+}
