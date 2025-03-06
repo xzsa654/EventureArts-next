204d797d91b219c5fbed0d727201a87a259432c7
=xzsa654 <=xzsa654@gmail.com>
2025-03-06 16:57:54 +0800

    fixed the search clear method

--- components/common/message-drawer.js/message.js
+++ components/common/message-drawer.js/message.js
@@ -90,6 +90,9 @@ export default function MessageDrawer({
                     onChange={(e) => {
                       setFilterValue(e.target.value)
                     }}
+                    onClear={() => {
+                      setFilterValue('')
+                    }}
                     startContent={
                       <FaSearch className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
                     }
