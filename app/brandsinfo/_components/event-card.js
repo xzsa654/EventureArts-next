854eeab58d26cbdd9f71ffbeec693ae0d8e471d7
lichia <lily850413@gmail.com>
2025-03-07 15:54:23 +0800

    準備event-card圖片路徑

--- app/brandsinfo/_components/event-card.js
+++ app/brandsinfo/_components/event-card.js
@@ -15,7 +15,9 @@ export default function EventCard({ event }) {
 
   // 生成圖片路徑
   const eventImage = event.image
-    ? `http://localhost:3001/uploads/chu-uploads/${event.image}`
+    ? event.type === 'exhibition'
+      ? `http://localhost:3001/uploads/chu-uploads/${event.image}`
+      : `https://localhost:3001/uploads/course/${event.image}`
     : 'https://heroui.com/images/hero-card-complete.jpeg' // 預設圖片
 
   // 動態決定跳轉網址
